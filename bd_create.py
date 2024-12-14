b3c08c095738b7532e0dac53acb66a456f3121dc
Sweta Sergeeva <sweta.sergeeva@gmail.com>
2024-12-15 00:06:01 +0300

    DZ3 telegram bot

---
+++ bd_create.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+# Создаем базу данных и таблицу
+conn = sqlite3.connect('school_data.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER,
+    grade TEXT
+)
+''')
+
+conn.commit()
+conn.close()
